5e938c10af4fc1b6844e8f61476fc35d22e9177f
tatyana521@gmail.com <tatyana521@gmail.com>
2021-06-18 00:27:29 +0000

    updated docs, tests, and sources

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,11 @@
+with customers as (
+    
+    select 
+        id as customer_id,
+        first_name,
+        last_name
+
+    from {{ source('raw', 'customers') }}
+)
+
+select * from customers
